6e41fccb7aa560ce2db743788bd06786e7c164c3
GDandTOP <larry8000@nate.com>
2022-02-13 22:55:54 +0900

    코테

--- Python 풀이/level 2/1436 영화감독 숌.py
+++ Python 풀이/level 2/1436 영화감독 숌.py
@@ -1,10 +1,14 @@
+
+
 n = int(input())
 
-name = 666
-cnt=0
-while(True):
-    if "666" in str(name) : 
+cnt = 0
+number=666
+
+while(1):
+    if '666' in str(number) :
         cnt+=1
-        if cnt == n : print(name) ; break
-    
-    name+=1
+        if cnt==n :
+            print(number)
+            break
+    number+=1
